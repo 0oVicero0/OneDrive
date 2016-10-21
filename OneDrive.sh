f3f6ddf6b7b6099f44c59c087c7726218fa05bb5
Vicer <0oVicero0@users.noreply.github.com>
2016-10-21 22:35:14 +0800

    Add files via upload

---
+++ OneDrive.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function Install() {
+REPATH=$(pwd)
+rm -rf /usr/local/etc/OneDrive/$TYPE
+mkdir -p /usr/local/etc/OneDrive/$TYPE
+cd /usr/local/etc/OneDrive/$TYPE
+wget --no-check-certificate -q -O json-parser https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/json-parser
+wget --no-check-certificate -q -O onedrive https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive
+wget --no-check-certificate -q -O onedrive-authorize https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive-authorize
+wget --no-check-certificate -q -O onedrive-base https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive-base
+wget --no-check-certificate -q -O onedrive.cfg https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive.cfg
+chmod -R +x /usr/local/etc/OneDrive/$TYPE
+ln -sf /usr/local/etc/OneDrive/$TYPE/onedrive /usr/local/bin
+cd $REPATH
+}
+
+if [[ "$1" == 'business' ]]; then
+TYPE='Business'
+Install;
+elif [[ "$1" == 'personal' ]]; then
+TYPE='Personal'
+Install;
+else
+echo -n "bash $0 [personal|business]";
+fi
+
