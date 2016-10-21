fb45aa774cca512edd9e595c5305baa799422719
Vicer <0oVicero0@users.noreply.github.com>
2016-10-21 23:38:06 +0800

    Update OneDrive.sh

--- OneDrive.sh
+++ OneDrive.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 
-ERROR_CODE1=0
-ERROR_CODE2=0
-ERROR_CODE3=0
-ERROR_CODE4=0
-ERROR_CODE5=0
-
-function ERROR() {
-[ "$ERROR_CODE1"|"$ERROR_CODE2"|"$ERROR_CODE3"|"$ERROR_CODE4"|"$ERROR_CODE5" ] || {
-echo "Install error! "
-}}
-
 function Install() {
 REPATH=$(pwd)
 rm -rf /usr/local/etc/OneDrive/$TYPE
 mkdir -p /usr/local/etc/OneDrive/$TYPE
 cd /usr/local/etc/OneDrive/$TYPE
-wget --no-check-certificate -q -O json-parser https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/json-parser && ERROR_CODE1=1
-wget --no-check-certificate -q -O onedrive https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive && ERROR_CODE2=1
-wget --no-check-certificate -q -O onedrive-authorize https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive-authorize && ERROR_CODE3=1
-wget --no-check-certificate -q -O onedrive-base https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive-base && ERROR_CODE4=1
-wget --no-check-certificate -q -O onedrive.cfg https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive.cfg && ERROR_CODE5=1
+wget --no-check-certificate -q -O json-parser https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/json-parser
+wget --no-check-certificate -q -O onedrive https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive
+wget --no-check-certificate -q -O onedrive-authorize https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive-authorize
+wget --no-check-certificate -q -O onedrive-base https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive-base
+wget --no-check-certificate -q -O onedrive.cfg https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive.cfg
 chmod -R +x /usr/local/etc/OneDrive/$TYPE
 ln -sf /usr/local/etc/OneDrive/$TYPE/onedrive /usr/local/bin
 cd $REPATH
