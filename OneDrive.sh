01ba5a3a067971564dd954505a63ce731be91567
Vicer <0oVicero0@users.noreply.github.com>
2016-10-31 16:08:32 +0800

    Update OneDrive.sh

--- OneDrive.sh
+++ OneDrive.sh
@@ -5,11 +5,11 @@ REPATH=$(pwd)
 rm -rf /usr/local/etc/OneDrive/$TYPE
 mkdir -p /usr/local/etc/OneDrive/$TYPE
 cd /usr/local/etc/OneDrive/$TYPE
-wget --no-check-certificate -q -O json-parser https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/json-parser
-wget --no-check-certificate -q -O onedrive https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive
-wget --no-check-certificate -q -O onedrive-authorize https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive-authorize
-wget --no-check-certificate -q -O onedrive-base https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive-base
-wget --no-check-certificate -q -O onedrive.cfg https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive.cfg
+wget --no-check-certificate -q -O json-parser "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/json-parser?token=AKo0rJwREqx_VfuEK7MzN150kVkhivHRks5YIDShwA=="
+wget --no-check-certificate -q -O onedrive "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive?token=AKo0rJwREqx_VfuEK7MzN150kVkhivHRks5YIDShwA=="
+wget --no-check-certificate -q -O onedrive-authorize "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive-authorize?token=AKo0rJwREqx_VfuEK7MzN150kVkhivHRks5YIDShwA=="
+wget --no-check-certificate -q -O onedrive-base "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive-base?token=AKo0rJwREqx_VfuEK7MzN150kVkhivHRks5YIDShwA=="
+wget --no-check-certificate -q -O onedrive.cfg "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive.cfg?token=AKo0rJwREqx_VfuEK7MzN150kVkhivHRks5YIDShwA=="
 chmod -R +x /usr/local/etc/OneDrive/$TYPE
 ln -sf /usr/local/etc/OneDrive/$TYPE/onedrive /usr/local/bin
 cd $REPATH
