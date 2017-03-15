15c69e9c907d2ab02ad302a2803cccfcbc1864f5
Vicer <0oVicero0@users.noreply.github.com>
2017-03-15 13:18:20 +0800

    Update OneDrive.sh

--- OneDrive.sh
+++ OneDrive.sh
@@ -2,27 +2,19 @@
 
 function Install() {
 REPATH=$(pwd)
-rm -rf /usr/local/etc/OneDrive/$TYPE
-mkdir -p /usr/local/etc/OneDrive/$TYPE
+rm -rf /usr/local/etc/OneDrive
+mkdir -p /usr/local/etc/OneDrive
 cd /usr/local/etc/OneDrive/$TYPE
-wget --no-check-certificate -q -O json-parser "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/json-parser"
-wget --no-check-certificate -q -O onedrive "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive"
-wget --no-check-certificate -q -O onedrive-authorize "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive-authorize"
-wget --no-check-certificate -q -O onedrive-base "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive-base"
-wget --no-check-certificate -q -O onedrive.cfg "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/$TYPE/onedrive.cfg"
-chmod -R +x /usr/local/etc/OneDrive/$TYPE
-ln -sf /usr/local/etc/OneDrive/$TYPE/onedrive /usr/local/bin
+wget --no-check-certificate -q -O json-parser "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/json-parser"
+wget --no-check-certificate -q -O onedrive "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/onedrive"
+wget --no-check-certificate -q -O onedrive-authorize "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/onedrive-authorize"
+wget --no-check-certificate -q -O onedrive-base "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/onedrive-base"
+wget --no-check-certificate -q -O onedrive.cfg "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/onedrive.cfg"
+chmod -R +x /usr/local/etc/OneDrive
+ln -sf /usr/local/etc/OneDrive/onedrive /usr/local/bin
 cd $REPATH
 }
 
-if [[ "$1" == 'business' ]]; then
-TYPE='Business'
 Install;
-elif [[ "$1" == 'personal' ]]; then
-TYPE='Personal'
-Install;
-else
-echo "bash $0 [personal|business]";
-fi
 
 rm -rf $(basename "$0")
