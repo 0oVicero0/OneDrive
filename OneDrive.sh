ed05795dc6ba6efcafa7c8dd67c460e0e1c59a48
Vicer <0oVicero0@users.noreply.github.com>
2017-07-07 12:40:37 +0800

    Update OneDrive.sh

--- OneDrive.sh
+++ OneDrive.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-function Install() {
-REPATH=$(pwd)
 rm -rf /usr/local/etc/OneDrive
 mkdir -p /usr/local/etc/OneDrive
-cd /usr/local/etc/OneDrive/$TYPE
+cd /usr/local/etc/OneDrive
 wget --no-check-certificate -q -O json-parser "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/json-parser"
 wget --no-check-certificate -q -O onedrive "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/onedrive"
 wget --no-check-certificate -q -O onedrive-authorize "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/onedrive-authorize"
@@ -12,9 +10,5 @@ wget --no-check-certificate -q -O onedrive-base "https://raw.githubusercontent.c
 wget --no-check-certificate -q -O onedrive.cfg "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/onedrive.cfg"
 chmod -R +x /usr/local/etc/OneDrive
 ln -sf /usr/local/etc/OneDrive/onedrive /usr/local/bin
-cd $REPATH
-}
-
-Install;
 
 rm -rf $(basename "$0")
