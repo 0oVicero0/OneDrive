643b6ca758a058f187af9a6ae84850ad205b41bd
Vicer <0oVicero0@users.noreply.github.com>
2018-11-19 12:54:11 +0800

    Update OneDrive.sh

--- OneDrive.sh
+++ OneDrive.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -rf /usr/local/etc/OneDrive
+[ -d /usr/local/etc/OneDrive ] && mv -f /usr/local/etc/OneDrive /usr/local/etc/OneDrive.bak
 mkdir -p /usr/local/etc/OneDrive
 cd /usr/local/etc/OneDrive
 wget --no-check-certificate -q -O json-parser "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/json-parser"
@@ -12,5 +12,9 @@ wget --no-check-certificate -q -O onedrive.cfg "https://raw.githubusercontent.co
 chmod -R a+x /usr/local/etc/OneDrive
 ln -sf /usr/local/etc/OneDrive/onedrive /usr/local/bin/
 ln -sf /usr/local/etc/OneDrive/onedrive-d /usr/local/bin/
+[ -d /usr/local/etc/OneDrive.bak ] && {
+[ -f /usr/local/etc/OneDrive.bak/onedrive.cfg ] && cp -rf /usr/local/etc/OneDrive.bak/onedrive.cfg /usr/local/etc/OneDrive/
+[ -f /usr/local/etc/OneDrive.bak/.refresh_token ] && cp -rf /usr/local/etc/OneDrive.bak/.refresh_token /usr/local/etc/OneDrive/
+}
 
 rm -rf $(basename "$0")
